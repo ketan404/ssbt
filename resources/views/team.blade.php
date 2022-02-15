c413b953f3244352d3e2cd4479ca11fb1ae51042
Tejaswini_Dumbare <70147704+teju-dumbare@users.noreply.github.com>
2022-02-15 23:37:13 +0530

    Update team.blade.php

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -23,26 +23,20 @@
         <div class="ml-12">
             <div class="mt-2 text-sm text-gray-500">
 		<div class="flex pt-8 sm:pt-0 justify-center">
-                    
-                      <img src="/images/ssbt1.jpg">
-                      <br>
-                      
+                      <img src="/images/ssbt1.jpg" />
                 </div>
 
             </div>
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"></t>Mahadeo s/o Pandurang Mother name:Shrimabai</div>
         </div>
     </div>
-    
 
     <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
         <div class="flex items-center">
-            <img src="/images/ssbt6.jpg"/>
-            
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Mr. Mahadeo Pandurang Shingare</div>
         </div>
-        <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Bapusaheb s/o Maruti Mother name:Housabai</div>
-
-<p></p>
+<p>House no. 4893 Rang Baba Mala, Azad Road, Kodali Panhala, Kolhapur-416114.</p>
+<p>Aadhar No. 982805346626</p>
+<p>Mobile.No.7038887426</p>
         <div class="ml-12">
             <div class="mt-2 text-sm text-gray-500">
 		<p style="text-align:justify; padding-right:3%;" >
@@ -59,31 +53,27 @@
             <div class="mt-2 text-sm text-gray-500">
 		<div class="flex justify-center pt-8  sm:pt-0">
                       <img src="/images/ssbt2.jpg" />
-                      
                 </div>
             </div>
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Sharda w/o Tukaram Mother name: Shrimabai</div>
         </div>
     </div>
 
     <div class="p-6 border-t border-gray-200 md:border-l">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"></div>
-            <img src="/images/ssbt3.jpg"/>
-            
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">So. Sampada Mahadeo Shingare</div>
         </div>
-        <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Sampada w/o Mahadeo Mother name: Bebitai</div>
 		<div class="ml-12">
             <div class="mt-2 text-sm text-gray-500">
 		<p style="text-align:justify; padding-right:3%;">
-		
+		Information here ...
                 </p>
 		<p style="text-align:justify; padding-right:2%;">
-		
+		more information here
                 </p>
-				<p>
+				<p>House No. 3663 Rang Baba Mala, Azad Road, Kodli Panhala, Kolhapur -416114.</p> 
+                 <p>BASE NO. 458886982181</p>
+<p>Mobile.NO.8806667426</p>
 
-</p>
             </div>
         </div>
     </div>
@@ -94,22 +84,113 @@
         <div class="ml-12">
             <div class="mt-2 text-sm text-gray-500">
 		<div class="flex justify-center pt-8  sm:pt-0">
-                      <img src="/images/ssbt4.jpg" />
-                      
+                      <img src="/images/ssbt3.jpg" />
                 </div>
             </div>
-            <div class="ml- text-lg text-gray-600 leading-7 font-semibold">Ameet s/o Mahadeo Mother name: Sampada</div>
         </div>
     </div>
 
     <div class="p-6 border-t border-gray-200 md:border-l">
         <div class="flex items-center">
-            <img src="/images/ssbt5.jpg"/>
-            
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">So. Sharda Tukaram Hazare</div>
         </div>
-        <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><p>Ajit s/o Mahadeo Mother name: Sampada</div>
-	<p></p>
+	<p>Dhangar Galli, Ankalkhop, Sangli 416316</P>
+<P>Aadhaar No.528972376266</p>
+<p>Mobile.No.8484975731</p>
 </div>
+<div class="p-6 border-t border-gray-200">
+        <div class="flex items-center">
+        </div>
 
+        <div class="ml-12">
+            <div class="mt-2 text-sm text-gray-500">
+		<div class="flex justify-center pt-8  sm:pt-0">
+                      <img src="/images/ssbt4.jpg" />
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="p-6 border-t border-gray-200 md:border-l">
+        <div class="flex items-center">
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Mr. Bapusaheb Maruti Suryavanshi</div>
+        </div>
+		<p>C-5, Shreyas Apartment, Ganesh Khind Road, Shivaji Nagar, Pune-411016.</p>
+            <p>BASE NO.631882162072</p> 
+<p>Mobile.No. 9881415282</P>
+		</div>
+		<div class="p-6 border-t border-gray-200">
+        <div class="flex items-center">
+        </div>
+
+        <div class="ml-12">
+            <div class="mt-2 text-sm text-gray-500">
+		<div class="flex justify-center pt-8  sm:pt-0">
+                      <img src="/images/ssbt5.jpg" />
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="p-6 border-t border-gray-200 md:border-l">
+        <div class="flex items-center">
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Mr. Ameet Mahadeo Shingare</p>
+</div>
+        </div>
+		<p>House no. 4893 Rang Baba Mala, Azad Road, Kohli Panhala, Kolhapur-416114.</p> 
+<p>Aadhaar No.921851587417</p>
+<p>Mobile.No.8806667426</p>
+
+</p>
+		</div>
+		<div class="p-6 border-t border-gray-200">
+        <div class="flex items-center">
+        </div>
+
+        <div class="ml-12">
+            <div class="mt-2 text-sm text-gray-500">
+		<div class="flex justify-center pt-8  sm:pt-0">
+                      <img src="/images/ssbt6.jpg" />
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="p-6 border-t border-gray-200 md:border-l">
+        <div class="flex items-center">
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Mr. Sunil Mahadeo Pawar</div>
+        </div>
+		<p>1346 / B, Lotar Galli, Kakati,Belgaum, Karnataka-191113.</p>
+<p>Aadhar NO.604645045118</p>
+<p>Mobile.No. 9731866787</p>
+
+		</div>
+        <div class="p-6 border-t border-gray-200">
+        <div class="flex items-center">
+        </div>
+
+        <div class="ml-12">
+            <div class="mt-2 text-sm text-gray-500">
+		<div class="flex justify-center pt-8  sm:pt-0">
+                      <img src="/images/default.jpg" />
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="p-6 border-t border-gray-200 md:border-l">
+        <div class="flex items-center">
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Mr. Ketan Srikant Kulkarni</div>
+        </div>
+		<p>Rahul Towers Neighbor 404, A-2, Rahul Residency, Paud Road, Pune-411038.</p>
+<p>Aadhar NO.690082499097</p> 
+<p>Mobile.No.9420121704</p>
+
+</p>
+		</div>
+
+
+
+		
             </div>
 @endsection
